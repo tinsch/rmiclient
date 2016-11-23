fe8c9c256bc821d94666bba02a96f08d27c063bc
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-11-23 19:24:12 +0100

    add tests for other calculations

--- src/rmi/client/Client.java
+++ src/rmi/client/Client.java
@@ -9,7 +9,10 @@ public class Client {
 	public static void main(String[] args) {
 		try {
 			CalculatorInterface c = (CalculatorInterface) Naming.lookup("rmi://localhost:1099/calculate"); //Proxy-Object
-			System.out.println(c.add(1,2));
+			System.out.println(c.add(1.0,2.0));
+			System.out.println(c.subtract(1.0,2.0));
+			System.out.println(c.multiply(1.0,2.0));
+			System.out.println(c.divide(1.0,2.0));
 		} catch (Exception e) {
 			System.out.println(e);
 		}
